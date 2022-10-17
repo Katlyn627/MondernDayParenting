88ddc665cbdf01038c4c7c6ae79ee973de1378b2
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-10-17 09:42:29 -0600

    setting up post columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,18 +8,25 @@ const posts = [
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto px-10 mb-8 bg-gray">
       <Head>
         <title>Parenting Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className= 'grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div>
         {posts.map((post, index) =>(
           <div>
             {post.title}
             {post.excerpt}
           </div>
         ))}
+        
+        </div>
+        
+        <div className='lg:col-span-4 col-span-1'>
+
+        </div>
 
 
       </div>
