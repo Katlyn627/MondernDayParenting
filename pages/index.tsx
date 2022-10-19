c374d5406b93de4a5f15de844bffd8edb055ab69
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-10-18 18:21:24 -0600

    creating base styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,8 @@ const Home: NextPage = () => {
         <div className='lg:col-span-4 col-span-1'>
           <div className= 'lg:sticky relative top-8'>
             <PostWidget/>
-            <Categories/>
+            <Categories />
+            {/* time 34:02 */}
 
           </div>
         </div>
