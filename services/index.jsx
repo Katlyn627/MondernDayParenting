97e653f5fc1190161c98275332b71d262b3e8754
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-10-19 09:20:02 -0600

    created Post Card to fetch data

--- services/index.jsx
+++ services/index.jsx
@@ -4,36 +4,37 @@ const graphqlAPI = process.env.NEXT_PUBLIC_PARENTINGBLOG_ENDPOINT
 
 export const getPosts = async () => {
     const query = gql`
-        query MyQuery {
-            postsConnection {
-                edges {
-                    node {
-                        author {
-                            bio
-                            name
-                            id
-                            photo {
-                                url
-                            }
-                        }
-                        createdAt
-                            slug
-                            title
-                            excerpt
-                            featuredImage {
-                                url
-                            }
-                            categories {
-                                name
-                                slug
-                            }
-                        }
-                    }
-                }         
-    }
-  `
-
-  const result = await request(graphqlAPI, query)
-
-  return result.postsConnection.edges
-}
+      query MyQuery {
+        postsConnection {
+          edges {
+            cursor
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query);
+  
+    return result.postsConnection.edges;
+  };
