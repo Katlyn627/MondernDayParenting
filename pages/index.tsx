8bf1e3ac06ec7879e52701585af4560ad9700687
Katlyn Boches <86095070+Katlyn627@users.noreply.github.com>
2022-10-18 20:35:34 -0600

    working of fetching data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,33 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import {PostCard, Categories, PostWidget} from '../components';
 
-const posts = [
-  { title: 'Terrible Twos', excerpt: 'What to Do at Two' },
-  { title: 'Nap Time', excerpt: 'Sleep When they Sleep' },
-];
+import { FeaturedPosts } from '../sections/index';
+import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const Home: NextPage = () => {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Parenting Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className= 'grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className= 'lg:col-span-8 col-span-1'>
-        {posts.map((post, index) => <PostCard post={post} key={post.title}/>)}
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
         </div>
-        <div className='lg:col-span-4 col-span-1'>
-          <div className= 'lg:sticky relative top-8'>
-            <PostWidget/>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
             <Categories />
-            {/* time 34:02 */}
-
           </div>
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Home
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
